925c8fd70e6b60bbfdbea1ec1f62f05c067163d5
Jan Pivarnik <pivarnikjan@gmail.com>
2016-12-14 22:36:26 +0100

    added crud functions in views

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_create(request):
+    return HttpResponse("<h1>Create</>")
+
+
+def post_detail(request):
+    return HttpResponse("<h1>Detail</>")
+
+
+def post_list(request):
+    return render(request, "index.html", {})
+
+
+def post_update(request):
+    return HttpResponse("<h1>Update</>")
+
+
+def post_delete(request):
+    return HttpResponse("<h1>Delete</>")
