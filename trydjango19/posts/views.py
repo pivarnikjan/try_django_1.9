9c856e227ebd8d1f6fa7445a42511563ee7889a0
Jan Pivarnik <pivarnikjan@gmail.com>
2016-12-14 23:24:56 +0100

    added names and namespaces

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,17 +1,38 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def post_create(request):
     return HttpResponse("<h1>Create</>")
 
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</>")
+def post_detail(request, id):
+    # instance = Post.objects.get(id=1)  # nebude fungovat
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):
-    return render(request, "index.html", {})
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    # else:
+    #     context = {
+    #         "title": "List"
+    #     }
+    return render(request, "index.html", context)
 
 
 def post_update(request):
